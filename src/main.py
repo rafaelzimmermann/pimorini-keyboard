edd66ebaea795d85415eb027f272ae16940ee78f
Rafael Zimmermann <engzimmermann@gmail.com>
2022-09-24 08:35:59 +0200

    Custom version of Pimoroni example

--- src/main.py
+++ src/main.py
@@ -1,53 +1,60 @@
 import time
 import picokeypad as keypad
 
-keypad.init()
-keypad.set_brightness(1.0)
-
-lit = 0
-last_button_states = 0
-colour_index = 0
-
 NUM_PADS = keypad.get_num_pads()
-while True:
-    button_states = keypad.get_button_states()
-    if last_button_states != button_states:
-        last_button_states = button_states
-        if button_states > 0:
-            if lit == 0xffff:
-                # all buttons are already lit, reset the test
-                lit = 0
-                colour_index += 1
-                if colour_index >= 6:
-                    colour_index = 0
-            else:
-                button = 0
-                for find in range(0, NUM_PADS):
-                    # check if this button is pressed and no other buttons are pressed
-                    if button_states & 0x01 > 0:
-                        if not (button_states & (~0x01)) > 0:
-                            lit = lit | (1 << button)
-                        break
-                    button_states >>= 1
-                    button += 1
+COLORS = [
+    [0x00, 0x20, 0x00],
+    [0x20, 0x20, 0x00],
+    [0x20, 0x00, 0x00],
+    [0x20, 0x00, 0x20],
+    [0x00, 0x00, 0x20],
+    [0x00, 0x20, 0x20],
+]
+COLOR_OFF = [0x05, 0x05, 0x05]
 
-    for i in range(0, NUM_PADS):
-        if (lit >> i) & 0x01:
-            if colour_index == 0:
-                keypad.illuminate(i, 0x00, 0x20, 0x00)
-            elif colour_index == 1:
-                keypad.illuminate(i, 0x20, 0x20, 0x00)
-            elif colour_index == 2:
-                keypad.illuminate(i, 0x20, 0x00, 0x00)
-            elif colour_index == 3:
-                keypad.illuminate(i, 0x20, 0x00, 0x20)
-            elif colour_index == 4:
-                keypad.illuminate(i, 0x00, 0x00, 0x20)
-            elif colour_index == 5:
-                keypad.illuminate(i, 0x00, 0x20, 0x20)
-        else:
-            keypad.illuminate(i, 0x05, 0x05, 0x05)
 
-    keypad.update()
+class Keyboard:
 
-    time.sleep(0.1)
+    def __init__(self):
+        keypad.init()
+        keypad.set_brightness(1.0)
+        self.color_state = [COLOR_OFF for _ in range(0, NUM_PADS)]
+        for i in range(0, NUM_PADS):
+            keypad.illuminate(*([i] + COLOR_OFF))
+            keypad.update()
+
+    def run(self):
+        while True:
+            state = self.button_states()
+            if any(state):
+                for i in range(0, NUM_PADS):
+                    if state[i] and self.color_state[i] == COLOR_OFF:
+                        self.lit(i, COLORS[i % 5])
+                    else:
+                        self.unlit(i)
+
+            keypad.update()
+            time.sleep(0.1)
+
+    def button_states(self):
+        state = []
+        bin_state = keypad.get_button_states()
+        for _ in range(0, NUM_PADS):
+            state.append(bin_state & 0x01 > 0)
+            bin_state >>= 1
+        return state
+
+    def lit(self, button, color):
+        keypad.illuminate(*([button] + color))
+
+    def unlit(self, button):
+        keypad.illuminate(*([button] + COLOR_OFF))
+
+    def unlit_all(self):
+        for i in range(0, NUM_PADS):
+            keypad.illuminate(*([i] + COLOR_OFF))
+            keypad.update()
+
+
+if __name__ == '__main__':
+    Keyboard().run()
