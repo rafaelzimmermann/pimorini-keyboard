504d7deab5f678aa141fff04e463d95513b1722c
Rafael Zimmermann <engzimmermann@gmail.com>
2022-10-23 11:18:47 +0200

    Move button color definition to config.py

--- src/code.py
+++ src/code.py
@@ -1,19 +1,7 @@
 import time
 from rainbow import Rainbow
 from rgb_keyboard import RGBKeyboard, NUM_PADS
-from config import key_action
-
-
-
-COLORS = [
-    [0x00, 0x20, 0x00],
-    [0x05, 0x05, 0x05],
-    [0x20, 0x00, 0x00],
-    [0x20, 0x00, 0x20],
-    [0x00, 0x00, 0x20],
-    [0x00, 0x20, 0x20],
-]
-COLOR_OFF = [0x20, 0x20, 0x00]
+from config import key_action, DEFAULT_COLOR
 
 
 class Keyboard:
@@ -21,9 +9,8 @@ class Keyboard:
     def __init__(self):
         self.keyboard = RGBKeyboard()
         self.rainbow = Rainbow(self.keyboard)
-        self.rainbow.color_loop()
-        self.rainbow.color_loop(COLORS[5])
-        self.color_state = [COLOR_OFF for _ in range(0, NUM_PADS)]
+        self.rainbow.color_loop(DEFAULT_COLOR)
+        self.color_state = [DEFAULT_COLOR for _ in range(0, NUM_PADS)]
         for i in range(0, 16):
             self.keyboard.on_button_press(i, key_action[i])
 
@@ -34,8 +21,6 @@ class Keyboard:
                 for i in range(0, NUM_PADS):
                     if state[i]:
                         self.keyboard.execute_action(i)
-                    else:
-                        self.keyboard.set_button_color(i, *COLOR_OFF)
 
                 self.keyboard.update()
             time.sleep(0.1)
